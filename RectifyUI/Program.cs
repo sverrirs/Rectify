83164ed1fe6646ba9739b35c8f33d30a6642d867
Sverrir Sigmundarson <sverrirs@gmail.com>
2016-06-11 22:20:22 +0200

    Adding costura dependency to merge linked dlls, fixing anoying language folders in build, embedding log4net config file

--- RectifyUI/Program.cs
+++ RectifyUI/Program.cs
@@ -18,11 +18,28 @@ namespace RectifyUI
         [STAThread]
         static void Main()
         {
-            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.config.xml"));
+            //log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.config.xml"));
+            log4net.Config.XmlConfigurator.Configure(GetLog4NewEmbeddedConfigFile("log4net.config.xml"));
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        private static Stream GetLog4NewEmbeddedConfigFile(string filename )
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            foreach (string resName in assembly.GetManifestResourceNames())
+            {
+                if (resName.EndsWith(filename))
+                {
+                    System.IO.StreamReader reader = new System.IO.StreamReader(assembly.GetManifestResourceStream(resName),
+                        System.Text.Encoding.UTF8);
+                    return reader.BaseStream;
+                }
+            }
+            return null;
         }
     }
 }
